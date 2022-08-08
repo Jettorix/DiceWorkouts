cb5a373bcb48d8cf6caab2e815714fe834119448
Jettorix <109848112+Jettorix@users.noreply.github.com>
2022-08-08 03:16:07 -0500

    Implemented new functions
    Added 2 new functions 
    Workout Error function tells the user that they can not change workouts without rolling the dice 
    The other function handles the dice roll, and redirecting the user to the right page based on its outcome. 
    That function will also tell the user what number they got on their dice roll.

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 
+function workouterror()
+{
+    alert("Click the dice to change workout")
+}
+
+function getRndInteger(min, max) {
+    let x = Math.floor(Math.random() * (max - min)) + min;
+    if(x == 6) {
+      alert("You got a 6" )
+      window.location="ex6.html"
+      
+    }
+    if(x == 1) {
+        alert("You got a 1" )
+        window.location="ex1.html"
+     
+    }
+    if(x == 2) {
+      
+      alert("You got a 2" )
+      window.location="ex2.html"
+
+    }
+    if(x == 3) {
+      alert("You got a 3" )
+      window.location="ex3.html"
+      
+    }
+    if(x == 4) {
+      alert("You got a 4" )
+      window.location="ex4.html"
+       
+    }
+    if(x == 5) {
+      alert("You got a 5" )
+      window.location="ex5.html"
+      
+    }
+  }
 
